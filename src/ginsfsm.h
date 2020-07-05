553e1ed189a30c1d1f3da6872bcc88c41151e759
Niyamaka <niyamaka@yuneta.io>
2020-07-05 18:13:12 +0200

    v4.2.2 tls ok, i think

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -32,7 +32,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.2.1"
+#define __ginsfsm_version__  "4.2.2"
 
 
 #ifdef __cplusplus
