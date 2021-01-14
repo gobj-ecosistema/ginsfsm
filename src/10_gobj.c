677e2cf8c533669cd01fc65871083c0593b138c4
Niyamaka <niyamaka@yuneta.io>
2021-01-14 10:15:15 +0100

    v4.6.7 "Working without authentication" cannot return -1 because deny access.

--- src/10_gobj.c
+++ src/10_gobj.c
@@ -11510,7 +11510,7 @@ PUBLIC json_t *gobj_authenticate(hgobj gobj_, json_t *kw, hgobj src)
 
         KW_DECREF(kw);
         return json_pack("{s:i, s:s, s:s}",
-            "result", -1,
+            "result", 0,
             "comment", "Working without authentication",
             "username", pw->pw_name
         );
