d74e98173d773c755a53bf1a41510c33df405318
Niyamaka <niyamaka@yuneta.io>
2021-02-21 21:51:06 +0100

    v4.9.8

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -33,7 +33,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.9.7"
+#define __ginsfsm_version__  "4.9.8"
 
 
 #ifdef __cplusplus
