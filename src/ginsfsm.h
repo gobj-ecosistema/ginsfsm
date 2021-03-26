946007d668a86dfc34996e38121e375c954c3245
Niyamaka <niyamaka@yuneta.io>
2021-03-26 09:09:26 +0100

    v4.10.16 fix openssl

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -33,7 +33,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.10.15"
+#define __ginsfsm_version__  "4.10.16"
 
 
 #ifdef __cplusplus
