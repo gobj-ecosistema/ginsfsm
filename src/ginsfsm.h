eb859da0dce698f88fdd4ae02c931387611ffdd7
Niyamaka <niyamaka@yuneta.io>
2021-06-28 10:33:56 +0200

    v4.15.0 warning persistent attributes big changes

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -33,7 +33,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.14.0"
+#define __ginsfsm_version__  "4.15.0"
 
 
 #ifdef __cplusplus
