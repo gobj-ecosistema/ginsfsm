caf08f2f9f2594d4fdec8c3f9c265dde6e2a0278
Niyamaka <niyamaka@yuneta.io>
2021-07-26 10:50:49 +0200

    fix node_owner

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.15.2"
+#define __ginsfsm_version__  "4.15.3"
 
 
 #ifdef __cplusplus
