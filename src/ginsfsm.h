c3dd5a7db42472caaa7a81c18a6a50b91ae236bc
Niyamaka <niyamaka@yuneta.io>
2021-07-31 18:13:27 +0200

    v4.15.4 Update liboauth2; full url of agent yunetacontrol: (^^__sys_machine__^^).(^^__node_owner__^^).(^^__output_url__^^); node_owner in yuneta_agent.json

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.15.3"
+#define __ginsfsm_version__  "4.15.4"
 
 
 #ifdef __cplusplus
