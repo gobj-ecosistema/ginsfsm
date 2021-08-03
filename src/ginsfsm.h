147e56a5b2447ce152b4f41095afc066f547d6ca
Niyamaka <niyamaka@yuneta.io>
2021-08-03 21:43:10 +0200

    v4.15.7 warning agent

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.15.6"
+#define __ginsfsm_version__  "4.15.7"
 
 
 #ifdef __cplusplus
