fa8bf1c1ce740fd397292c385170a3e41cbcaf5d
Niyamaka <niyamaka@yuneta.io>
2021-09-08 12:14:17 +0200

    v4.16.1 change realm_name by realm_role

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.16.0"
+#define __ginsfsm_version__  "4.16.1"
 
 
 #ifdef __cplusplus
