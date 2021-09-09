447ea0222c66d5bd755e3526314a710dbc815e8b
Niyamaka <niyamaka@yuneta.io>
2021-09-09 15:17:49 +0200

    v4.17.1 dba_posgres: se crea automaticamente el campo en las tablas: __created_at__

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.17.0"
+#define __ginsfsm_version__  "4.17.1"
 
 
 #ifdef __cplusplus
