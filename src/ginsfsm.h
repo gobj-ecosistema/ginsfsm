4266e994f0628ad155ae6f7ffd755372fdcb897c
Niyamaka <niyamaka@yuneta.io>
2021-09-12 14:22:07 +0200

    v4.17.3 Authz - don't check iat field of jwt (verify.iat=skip), more log for dba_postgres

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.17.2"
+#define __ginsfsm_version__  "4.17.3"
 
 
 #ifdef __cplusplus
