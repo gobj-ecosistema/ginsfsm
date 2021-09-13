76b29e64abe520986a9b5777881348e3513d3891
Niyamaka <niyamaka@yuneta.io>
2021-09-13 12:37:36 +0200

    v4.17.5 node_uuid() get uuid of disk partition more old

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.17.4"
+#define __ginsfsm_version__  "4.17.5"
 
 
 #ifdef __cplusplus
