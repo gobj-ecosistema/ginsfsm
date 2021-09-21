b76d2966592c5b27ec7a2c7824797e8635b832b9
Niyamaka <niyamaka@yuneta.io>
2021-09-21 11:16:07 +0200

    fix bug cli

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.19.0"
+#define __ginsfsm_version__  "4.19.1"
 
 
 #ifdef __cplusplus
