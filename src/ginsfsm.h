a47708f86c5a314bcf6d55b8a0f19e417e13fe7a
Niyamaka <niyamaka@yuneta.io>
2021-10-08 10:37:21 +0200

    v4.20.3 fix postgres

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.20.2"
+#define __ginsfsm_version__  "4.20.3"
 
 
 #ifdef __cplusplus
