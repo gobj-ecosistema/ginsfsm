10f114ac330b1210211a1fb983a05c9cd900c27c
Niyamaka <niyamaka@yuneta.io>
2021-11-03 11:18:42 +0100

    v4.22.1 FIX register/find public services in agent

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "4.22.0"
+#define __ginsfsm_version__  "4.22.1"
 
 
 #ifdef __cplusplus
