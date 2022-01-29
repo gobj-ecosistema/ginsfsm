96480eb780b0dd5ad8eedb55e9f809685edca7d5
Niyamaka <niyamaka@yuneta.io>
2022-01-29 11:14:30 +0100

    v5.1.6

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "5.1.5"  /* XX__yuneta_version__XX */
+#define __ginsfsm_version__  "5.1.6"  /* XX__yuneta_version__XX */
 
 
 #ifdef __cplusplus
