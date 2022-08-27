0553ef6151e93d2690341279dc62352117fb0699
ginsmar <ginsmar@gmail.com>
2022-08-27 11:24:08 +0200

    v5.12.4

--- src/ginsfsm.h
+++ src/ginsfsm.h
@@ -30,7 +30,7 @@ PUBLIC void end_ginsfsm_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ginsfsm_version__  "5.12.3"  /* XX__yuneta_version__XX */
+#define __ginsfsm_version__  "5.12.4"  /* XX__yuneta_version__XX */
 
 
 #ifdef __cplusplus
